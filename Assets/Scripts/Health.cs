ff29fa7b5132347229e3e116344665926ca8204f
Wangld5 <wangld4195@163.com>
2018-06-26 19:53:36 +0800

    Update Health.cs

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -46,7 +46,7 @@ public class Health : NetworkBehaviour {
 			{
 				currentHealth = maxHealth;
 
-				// called on the Server, will be invoked on the Clients
+
 				RpcRespawn();
 			}
 		}
@@ -62,16 +62,15 @@ public class Health : NetworkBehaviour {
 	{
 		if (isLocalPlayer)
 		{
-			// Set the spawn point to origin as a default value
+
 			Vector3 spawnPoint = Vector3.zero;
 
-			// If there is a spawn point array and the array is not empty, pick one at random
+
 			if (spawnPoints != null && spawnPoints.Length > 0)
 			{
 				spawnPoint = spawnPoints[Random.Range(0, spawnPoints.Length)].transform.position;
 			}
 
-			// Set the player’s position to the chosen spawn point
 			transform.position = spawnPoint;
 
 		}
